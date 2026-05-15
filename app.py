272bb9f8be6e687498ca01badf5f6870cc1869cc
Nigar Sahla <nigarsahla666@gmail.com>
2026-05-15 12:35:09 +0530

    Step 10 Complete: Added User Login + Signup System

--- app.py
+++ app.py
@@ -1,61 +1,99 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tera-super-secret-key-12345' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bug_tracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# Ye list me sare bugs store honge
-bugs_list = []
-bug_id_counter = 1
+# Flask-Login setup
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# User Model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# Bug Model - purana wala
+class Bug(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(20), default='Pending')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Routes
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if User.query.filter_by(username=username).first():
+            flash('Username already exists')
+            return redirect(url_for('signup'))
+            
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created! Please login.')
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/')
-def home():
-    return render_template('index.html', bugs=bugs_list)
+@login_required
+def index():
+    bugs = Bug.query.all()
+    return render_template('index.html', bugs=bugs, name=current_user.username)
 
 @app.route('/submit', methods=['POST'])
+@login_required
 def submit():
-    global bug_id_counter
     title = request.form['title']
-    desc = request.form['description']
-    
-    # Naya bug list me add kar diya
-    bugs_list.append({
-        'id': bug_id_counter,
-        'title': title,
-        'description': desc,
-        'status': 'Pending'  # Default status
-    })
-    bug_id_counter += 1
-    
-    return redirect('/')  # Wapas home page pe bhej do
-@app.route('/delete/<int:bug_id>')
-def delete_bug(bug_id):
-    global bugs_list
-    bugs_list = [bug for bug in bugs_list if bug['id'] != bug_id]
-    return redirect('/')
-
-@app.route('/status/<int:bug_id>/<new_status>')
-def update_status(bug_id, new_status):
-    for bug in bugs_list:
-        if bug['id'] == bug_id:
-            bug['status'] = new_status
-            break
-    return redirect('/')
-@app.route('/edit/<int:bug_id>')
-def edit_bug(bug_id):
-    bug_to_edit = None
-    for bug in bugs_list:
-        if bug['id'] == bug_id:
-            bug_to_edit = bug
-            break
-    return render_template('edit.html', bug=bug_to_edit)
-
-@app.route('/update/<int:bug_id>', methods=['POST'])
-def update_bug(bug_id):
-    for bug in bugs_list:
-        if bug['id'] == bug_id:
-            bug['title'] = request.form['title']
-            bug['description'] = request.form['description']
-            break
-    return redirect('/')
+    description = request.form['description']
+    new_bug = Bug(title=title, description=description)
+    db.session.add(new_bug)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
