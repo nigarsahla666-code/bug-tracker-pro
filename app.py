1383290f835d462c7f55552f6151bfa792ceafed
nigarsahla666-code <nigarsahla666@gmail.com>
2026-05-15 19:44:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,8 @@ def submit():
     db.session.commit()
     return redirect(url_for('index'))
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=10000)
